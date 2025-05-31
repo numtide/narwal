1cb060bc5f22aa6530b89a6d4265ffdbcd855359
Brian McGee <brian@bmcgee.ie>
2025-05-31 10:33:52 +0100

    feat: init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nix Binary Cache";
+
+  # Add all your dependencies here
+  inputs = {
+
+    blueprint = {
+      url = "github:numtide/blueprint";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # Load the blueprint
+  outputs =
+    inputs:
+    inputs.blueprint {
+      prefix = "nix/";
+      inherit inputs;
+    };
+}
---
+++ nix/formatter.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  flake,
+  inputs,
+}:
+let
+  mod = inputs.treefmt-nix.lib.evalModule pkgs {
+    projectRootFile = ".git/config";
+
+    programs =
+      {
+        nixfmt.enable = true;
+        deadnix.enable = true;
+        gofumpt.enable = true;
+        prettier.enable = true;
+        statix.enable = true;
+      }
+      // pkgs.lib.optionalAttrs (pkgs.system != "riscv64-linux") {
+        shellcheck.enable = true;
+      };
+
+    settings = {
+      global.excludes = [
+        "LICENSE"
+        # unsupported extensions
+        "*.{gif,png,svg,tape,mts,lock,mod,sum,toml,env,envrc,gitignore}"
+      ];
+
+      formatter = {
+        deadnix = {
+          priority = 1;
+        };
+
+        statix = {
+          priority = 2;
+        };
+
+        nixfmt = {
+          priority = 3;
+        };
+
+        prettier = {
+          options = [
+            "--tab-width"
+            "4"
+          ];
+          includes = [ "*.{css,html,js,json,jsx,md,mdx,scss,ts,yaml}" ];
+        };
+      };
+    };
+  };
+in
+mod.config.build.wrapper
+// {
+  passthru.tests.check = mod.config.build.check flake;
+}
