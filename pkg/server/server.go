73a6a089c98ed74f7705253b123e08b1d62d39af
Brian McGee <brian@bmcgee.ie>
2025-12-02 16:19:04 +0000

    feat: remove http server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,17 +2,12 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"runtime"
 	"time"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/numtide/narwal/pkg/awssdk"
-	"github.com/numtide/narwal/pkg/store"
-
 	"github.com/charmbracelet/log"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/numtide/narwal/pkg/config"
-	"github.com/numtide/narwal/pkg/http"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -24,11 +19,7 @@ type Server struct {
 	log    *log.Logger
 	config *config.Server
 
-	pgPool   *pgxpool.Pool
-	s3Client *awssdk.BucketClient
-
-	http  *http.Server
-	store *store.Store
+	pgPool *pgxpool.Pool
 
 	eg *errgroup.Group // for background tasks
 }
@@ -56,29 +47,10 @@ func NewServer(cfg *config.Server) (*Server, error) {
 		return nil, err
 	}
 
-	// connect to s3
-	if srv.s3Client, err = cfg.S3.Connect(ctx); err != nil {
-		//nolint:wrapcheck
-		return nil, err
-	}
-
-	// create a store
-	if srv.store, err = store.New(cfg, srv.pgPool, srv.s3Client.UnderlyingClient(), srv.eg); err != nil {
-		return nil, fmt.Errorf("failed to create store: %w", err)
-	}
-
-	// create http server
-	if srv.http, err = http.NewServer(cfg, srv.store); err != nil {
-		return nil, fmt.Errorf("failed to create http server: %w", err)
-	}
-
 	return srv, nil
 }
 
 func (s *Server) Start(_ context.Context) error {
-	// start the http server
-	s.eg.Go(s.http.Listen)
-
 	s.log.Info("started")
 
 	return nil
@@ -86,19 +58,9 @@ func (s *Server) Start(_ context.Context) error {
 
 // Stop gracefully stops the server.
 func (s *Server) Stop(ctx context.Context) error {
-	// stop http server
-	if err := s.http.Stop(ctx); err != nil {
-		s.log.Error("failed to stop http server", "err", err)
-	}
-
 	// wait for background tasks to finish
 	if err := s.eg.Wait(); err != nil {
 		s.log.Error("failure occurred waiting for background tasks", "err", err)
-	}
-
-	// close the store
-	if err := s.store.Close(); err != nil {
-		s.log.Error("failed to close store", "err", err)
 	}
 
 	s.log.Info("stopped")
