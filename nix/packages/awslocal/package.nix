eeb0b1850cb41ada6cadedc84749d13d17e2f744
Brian McGee <brian@bmcgee.ie>
2025-12-02 12:08:16 +0000

    feat: add awslocal package
    Useful for interacting with localstack

---
+++ nix/packages/awslocal/package.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+
+  # build-system
+  setuptools,
+
+  # dependencies
+  localstack-client,
+  awscli,
+}:
+
+buildPythonPackage rec {
+  pname = "awscli-local";
+  version = "0.22.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "awscli_local";
+    inherit version;
+    hash = "sha256-B8Uyw3J1O/XxVCZFHckdbuyd6HeXSASTKamogr2sigs=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    localstack-client
+    awscli
+  ];
+
+  # No tests in the package
+  doCheck = false;
+
+  # No Python modules to import - just a script
+  dontUsePythonImportsCheck = true;
+
+  meta = {
+    description = "Thin wrapper around the 'aws' command line interface for use with LocalStack";
+    homepage = "https://github.com/localstack/awscli-local";
+    license = lib.licenses.asl20;
+    mainProgram = "awslocal";
+    maintainers = [ ];
+  };
+}
