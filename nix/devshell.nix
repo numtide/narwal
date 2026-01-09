ebdc1dfe692683ab68e33a1229aef29b44fecfcd
Brian McGee <brian@bmcgee.ie>
2026-01-09 09:46:19 +0000

    feat: expose our wrapped clickhouse-local as a flake package

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -4,44 +4,6 @@
   perSystem,
   ...
 }:
-let
-
-  # Wrap clickhouse-local to use $PRJ_DATA_DIR/clickhouse with UDFs from project
-  # Concatenate all SQL UDF files into a single init file at build time
-  clickhouse-sql-init = pkgs.runCommand "clickhouse-sql-init" { } ''
-    cat ${../clickhouse/user_defined}/*.sql > $out
-  '';
-
-  clickhouse-local-wrapped = pkgs.writeShellScriptBin "clickhouse-local" ''
-    CH_DATA_DIR="$PRJ_DATA_DIR/clickhouse"
-    mkdir -p "$CH_DATA_DIR"
-
-    # Generate config with absolute paths
-    cat > "$CH_DATA_DIR/config.xml" <<EOF
-    <clickhouse>
-        <user_scripts_path>${../clickhouse/user_scripts}</user_scripts_path>
-        <user_defined_executable_functions_config>${../clickhouse/user_defined}/*.xml</user_defined_executable_functions_config>
-    </clickhouse>
-    EOF
-
-    # If user passes --query or -q, run non-interactively with init file first
-    # Otherwise, run interactively
-    if [[ "$*" == *"--query"* ]] || [[ "$*" == *"-q"* ]]; then
-      exec ${pkgs.clickhouse}/bin/clickhouse-local \
-        --path="$CH_DATA_DIR" \
-        --config-file="$CH_DATA_DIR/config.xml" \
-        --queries-file="${clickhouse-sql-init}" \
-        "$@"
-    else
-      exec ${pkgs.clickhouse}/bin/clickhouse-local \
-        --path="$CH_DATA_DIR" \
-        --config-file="$CH_DATA_DIR/config.xml" \
-        --queries-file="${clickhouse-sql-init}" \
-        --interactive \
-        "$@"
-    fi
-  '';
-in
 perSystem.self.narwal.overrideAttrs (old: {
   env = old.env // {
     GOROOT = "${old.passthru.go}/share/go";
@@ -75,7 +37,7 @@ perSystem.self.narwal.overrideAttrs (old: {
       sqlc
     ])
     ++ [
-      clickhouse-local-wrapped
+      perSystem.self.clickhouse-local
     ];
 
   shellHook = ''
